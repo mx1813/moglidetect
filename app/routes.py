6a5a0933b407800f015e52c005c2b9354df00229
mx1813 <derjemand3@gmail.com>
2024-07-05 09:53:13 +0200

    flask tutorial step 2

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Hugo Habicht'}
+    posts = [
+        {
+            'author': {'username': 'MerdGüller'},
+            'body': 'Der Review-Bomber der Nation meldet sich zurück!'
+        },
+        {
+            'author': {'username': 'der71er'},
+            'body': 'Knorke Sache mein Freund!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
