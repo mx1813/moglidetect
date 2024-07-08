5a5c08f16e57fa2971e9509e630587995737b563
mx1813 <derjemand3@gmail.com>
2024-07-08 10:11:31 +0200

    progress bar example

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 import json
-from flask import Flask, render_template, jsonify, request, url_for
+import time
+from flask import Flask, render_template, jsonify, request, Response
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField, SelectField
 from wtforms.validators import DataRequired, Length
@@ -22,6 +23,7 @@ def index():
         if form.validate_on_submit():
             detectMethod = form.detectmethod.data
             input = form.inputText.data
+            certainty = 0
             print(detectMethod)
             if detectMethod == '1':
                 print('Detection with Compression Method')
@@ -36,22 +38,17 @@ def index():
             return jsonify(data=form.errors)
     return render_template('index.html', form=form)
 
-@app.route('/api/comprendetect', methods=['POST'])
-def login():
-    form = InputForm()
-    if form.validate_on_submit():
-        detectMethod = form.detectmethod.data
-        input = form.inputText.data
-        print(detectMethod)
-        if(detectMethod == 4):
-            z = comprendetect.EnsembledZippy().run_on_text_chunked(input)
-        elif(detectMethod == 1 or detectMethod == 2 or detectMethod == 3):
-            print('Heloookoko')
-            z = comprendetect.Zippy(detectMethod).run_on_text_chunked(input)
-        else:
-            z = 'not yet implemented'
-        return f'Ergebnis: {z} <br> Zu überprüfender Text: {input}'
-    return render_template('index.html', form=form)
+@app.route('/progress')
+def progress():
+	def generate():
+		x = 0
+		
+		while x <= 100:
+			yield "data:" + str(x) + "\n\n"
+			x = x + 10
+			time.sleep(0.5)
+
+	return Response(generate(), mimetype= 'text/event-stream')
 
 if __name__ == "__main__":
     app.run()
