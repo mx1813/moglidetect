802d1aaa0509aed38d75be2f65e41fdb0a3f2905
mx1813 <derjemand3@gmail.com>
2024-07-09 10:23:29 +0200

    set form validation

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import time
 from flask import Flask, render_template, jsonify, request, Response
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField, SelectField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import DataRequired, Length, InputRequired
 from comprendetect import comprendetect
 
 app = Flask(__name__)
@@ -12,31 +12,28 @@ app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY") or 'secretKey'
 
 class InputForm(FlaskForm):
     detectmethod = SelectField('Methode', choices=[('1', 'Kompression'), ('ft', 'Fine-Tuned GBERT'), ('ensemble', 'Alle')], validators=[DataRequired()])
-    inputText = TextAreaField('EingabeText', validators=[DataRequired(), Length(min=1, max=2048)])
+    inputText = TextAreaField('EingabeText', validators=[InputRequired(message="Du musst einen Text eingeben!"), Length(min=1, max=2048)])
     submit = SubmitField('Check Text!')
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def index():
     form = InputForm()
-    if request.method == "POST":
-        if form.validate_on_submit():
-            detectMethod = form.detectmethod.data
-            input = form.inputText.data
-            certainty = 0
-            print(detectMethod)
-            if detectMethod == '1':
-                print('Detection with Compression Method')
-                jsonString = comprendetect.EnsembledZippy().run_on_text_chunked(input)
-                jsonObject = json.loads(jsonString)
-                certValue = round(jsonObject["certainty"], 4)
-                certainty = round(certValue * 100, 2)
-                result = jsonObject["label"]
-            else:
-                result = 'not yet implemented'
-            return render_template('index.html', form=form, result=result, certainty=certainty)
+    if request.method == "POST" and form.validate_on_submit():
+        detectMethod = form.detectmethod.data
+        input = form.inputText.data
+        certainty = 0
+        print(detectMethod)
+        if detectMethod == '1':
+            print('Detection with Compression Method')
+            jsonString = comprendetect.EnsembledZippy().run_on_text_chunked(input)
+            jsonObject = json.loads(jsonString)
+            certValue = round(jsonObject["certainty"], 4)
+            certainty = round(certValue * 100, 2)
+            result = jsonObject["label"]
         else:
-            return jsonify(data=form.errors)
+            result = 'not yet implemented'
+        return render_template('index.html', form=form, result=result, certainty=certainty)
     return render_template('index.html', form=form)
 
 @app.route('/progress')
