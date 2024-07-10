8b51ef58063bead2c264b30f8a4273e17006d2a1
mx1813 <derjemand3@gmail.com>
2024-07-10 12:07:55 +0200

    enable async processing

--- app.py
+++ app.py
@@ -1,15 +1,55 @@
 import os
 import json
 import time
-from flask import Flask, render_template, jsonify, request, Response
+import random
+from flask import Flask, render_template, request, jsonify, url_for
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField, SelectField
+from flask_executor import Executor, futures
+from wtforms import SubmitField, TextAreaField, SelectField
 from wtforms.validators import DataRequired, Length, InputRequired
 from comprendetect import comprendetect
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY") or 'secretKey'
 
+executor = Executor(app)
+
+def compressionDetection(inputText):
+    print("start compression detection")
+    print(inputText)
+    jsonString = comprendetect.EnsembledZippy().run_on_text_chunked(inputText)
+    jsonObject = json.loads(jsonString)
+    certValue = round(jsonObject["certainty"], 4)
+    certainty = round(certValue * 100, 2)
+    certainty = round(certValue * 100, 2) + 50
+    if certainty > 100:
+            certainty = 99.87
+    responseList = {'result':jsonObject["label"], 'certainty':certainty}
+    responseJson = json.dumps(responseList)
+    print(responseJson)
+    return responseJson
+
+@app.route('/start_task')
+def long_task(self):
+    """Background task that runs a long function with progress reports."""
+    verb = ['Starting up', 'Booting', 'Repairing', 'Loading', 'Checking']
+    adjective = ['master', 'radiant', 'silent', 'harmonic', 'fast']
+    noun = ['solar array', 'particle reshaper', 'cosmic ray', 'orbiter', 'bit']
+    message = ''
+    total = random.randint(10, 50)
+    for i in range(total):
+        if not message or random.random() < 0.25:
+            message = '{0} {1} {2}...'.format(random.choice(verb),
+                                              random.choice(adjective),
+                                              random.choice(noun))
+        self.update_state(state='PROGRESS',
+                          meta={'current': i, 'total': total,
+                                'status': message})
+        time.sleep(1)
+    return {'current': 100, 'total': 100, 'status': 'Task completed!',
+            'result': 42}
+      
 class InputForm(FlaskForm):
     detectmethod = SelectField('Methode', choices=[('1', 'Kompression'), ('ft', 'Fine-Tuned GBERT'), ('ensemble', 'Alle')], validators=[DataRequired()])
     inputText = TextAreaField('EingabeText', validators=[InputRequired(message="Du musst einen Text eingeben!"), Length(min=1, max=2048)])
@@ -23,33 +63,33 @@ def index():
         detectMethod = form.detectmethod.data
         input = form.inputText.data
         certainty = 0
+        result = 'Loading'
         print(detectMethod)
         if detectMethod == '1':
             print('Detection with Compression Method')
-            jsonString = comprendetect.EnsembledZippy().run_on_text_chunked(input)
-            jsonObject = json.loads(jsonString)
-            certValue = round(jsonObject["certainty"], 4)
-            certainty = round(certValue * 100, 2)
-            certainty = round(certValue * 100, 2) + 50
-            if certainty > 100:
-                    certainty = 99.87
-            result = jsonObject["label"]
+            #task = long_task.apply_async()
+            executor.submit_stored('compression', compressionDetection, input)
+            print('Started executor')
         else:
             result = 'not yet implemented'
         return render_template('index.html', form=form, result=result, certainty=certainty)
     return render_template('index.html', form=form)
 
-@app.route('/progress')
-def progress():
-	def generate():
-		x = 0
-		
-		while x <= 100:
-			yield "data:" + str(x) + "\n\n"
-			x = x + 10
-			time.sleep(0.5)
+# @app.route('/update-status/<int:state>', methods=['PUT'])
+# def update_status(state: int):
+#     global status
+#     status += 30
+#     return '', 200
 
-	return Response(generate(), mimetype= 'text/event-stream')
+@app.route('/get-result')
+def get_result():
+    if not executor.futures.done('compression'):
+        state = executor.futures._state('compression')
+        return jsonify({'status': state})
+    future = executor.futures.pop('compression')
+    return jsonify({'status': 'done', 'result': future.result()})
+
+
 
 if __name__ == "__main__":
     app.run()
