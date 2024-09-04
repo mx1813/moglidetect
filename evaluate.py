8137cd5ba0514491113663e7af48d8ceeb0aaa72
mx1813 <derjemand3@gmail.com>
2024-09-04 15:13:24 +0200

    update models

--- evaluate.py
+++ evaluate.py
@@ -17,9 +17,9 @@ tn = []
 fp = []
 fn = []
 
-with open("evaLLM.csv", 'w', newline='\n', encoding='utf-8') as csvfile:
-    writer = csv.writer(csvfile, quotechar='"', delimiter=';')
-    field = ["true_label", "assigned_label", "result", "score"]
+with open("evaLLMK.csv", 'w', newline='\n') as csvfile:
+    writer = csv.writer(csvfile, quotechar='"', delimiter=',')
+    field = ["true_label", "assigned_label", "score"]
     writer.writerow(field)
     # Write each paragraph as a new row in the CSV
     i=1
@@ -32,15 +32,18 @@ with open("evaLLM.csv", 'w', newline='\n', encoding='utf-8') as csvfile:
     for text in evaDs:
         print(i)
         inputText = text['text']
-        jsonObject = llm_pipeline(inputText[:2048])
+        jsonObject = llm_pipeline_dbmz(inputText[:2048])
         #result, n_tokens = detector.score(inputText[:2048])
         #jsonString = ensembleDetection(inputText[:2048])
         #jsonObject = json.loads(jsonString)
+        #jsonObject = json.loads(comprendetect.EnsembledZippy().run_on_text_chunked(inputText))
+        score = jsonObject['score']
         print(jsonObject)
-        if jsonObject['label'] == 'KI' or jsonObject['label'] == 'AI':
+        if jsonObject['label'] == 'AI':
             ass_label = 1
         else: 
             ass_label = 0
+            score = 1 - jsonObject['score']
         print(text['label'])
         if text['label'] == 0:
             if ass_label == 0:
@@ -57,7 +60,7 @@ with open("evaLLM.csv", 'w', newline='\n', encoding='utf-8') as csvfile:
                 result = 'FN'
                 fn.append(i)
         print(result)
-        writer.writerow([text['label'], ass_label, result, jsonObject['score']])
+        writer.writerow([text['label'], ass_label, score])
         i+=1
         
 print("Ergebnisse LLM detection")
--- fine-tuning/finetuning_mogli.py
+++ fine-tuning/finetuning_mogli.py
@@ -8,32 +8,28 @@ from datasets import DatasetDict
 
 """Im ersten Schritt wird das Datenset, welches die Trainingsdaten enthält, geladen. Der Datenatz umfasst Texte und ordnet diesem einem Label zu (0=menschlich; 1=KI). In diesem Fall liegen die Daten in einer csv-Datei vor. Die Datei wird mithilfe von pandas eingelesen."""
 
-data = pd.read_csv('dsFull.csv', on_bad_lines='skip', sep=";", encoding="utf-8")
+data = pd.read_csv('dsTRAIN.csv', on_bad_lines='skip', sep=";", encoding="utf-8")
 print(data.head())
 
 """Der Datensatz werden in 3 Datensätze aufgeteilt: Train, Test und Validate. Anschließend werden die datensätze für das Training vorbereitet."""
 
-validate, train, test  = \
+train, test  = \
             np.split(data.sample(frac=1, random_state=42),
-                    [int(.1*len(data)), int(.8*len(data))])
+                    [int(.8*len(data))])
 
 
 # data preparation
 ## reset indices
 train = train.reset_index()[['text','label']]
 test = test.reset_index()[['text','label']]
-validate = validate.reset_index()[['text','label']]
 ## dataframes to datadict
 tds = Dataset.from_pandas(train)
 testds = Dataset.from_pandas(test)
-vds = Dataset.from_pandas(validate)
 tds.to_csv("dsTrain.csv", sep=";", index=False, encoding="utf-8")
 testds.to_csv("dsTest.csv", sep=";", index=False, encoding="utf-8")
-vds.to_csv("dsValidate.csv", sep=";", index=False, encoding="utf-8")
 dataset = DatasetDict()
 dataset['train'] = tds
 dataset['test'] = testds
-dataset['validate'] = vds
 print(dataset)
 
 """Anschließend werden die Datensätze tokenisiert, damit das vortrainierte Modell diese verwenden kann."""
@@ -62,11 +58,11 @@ def compute_metrics(eval_pred):
 model = AutoModelForSequenceClassification.from_pretrained('deepset/gbert-base', num_labels=2, id2label=id2label, label2id=label2id) # Pre-trained model
 
 training_args = TrainingArguments(
-    output_dir="mogli02",
+    output_dir="mogli05",
     learning_rate=2e-5,
     per_device_train_batch_size=32,
     per_device_eval_batch_size=32,
-    num_train_epochs=3,
+    num_train_epochs=2,
     weight_decay=0.01,
     eval_strategy="epoch",
     save_strategy="epoch",
@@ -90,4 +86,4 @@ trainer.train()
 
 """Nach dem Training das Modell nochmal in Ordner abspeichern"""
 
-trainer.save_model("mogli02_sec")
+trainer.save_model("mogli_gbert")
